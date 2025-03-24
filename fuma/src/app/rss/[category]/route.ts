b16443dd7d403d58f29512e04edb9fabe39aa1d1
ktKongTong <kt@ktlab.io>
2025-03-24 22:45:39 +0800

    Refactor RSS feed generation and move related functions to rss.ts

--- fuma/src/app/rss/[category]/route.ts
+++ fuma/src/app/rss/[category]/route.ts
@@ -1,35 +1,4 @@
-import { Feed } from "feed";
-import { source } from '@/lib/source'
-import { config } from '../../../../config'
-function generateRssFeed(category: string) {
-  const site = config.baseUrl
-  const feedOptions = {
-    title: config.feed?.title ?? `${config.title} | RSS Feed`,
-    description: config.feed?.description ?? `${category} RSS feed powered by Auto2Doc`,
-    id: config.feed?.id ??  site,
-    link: config.feed?.link ?? site,
-    language: config.feed?.language ?? "zh-CN",
-    image: config.feed?.image ?? `${site}/logo.png`,
-    favicon: config.feed?.favicon ?? `${site}/favicon.ico`,
-    copyright: config.feed?.copyright ?? 'Auto2Doc',
-  };
-  const feed = new Feed(feedOptions);
-  return feed;
-}
-
-
-
-const tree = source.getPageTree()
-
-type Node = (typeof tree.children)[number]
-type Item = Node & { type: 'page' }
-function dfs(pages: Item[], node: Node) {
-  switch (node.type) {
-    case 'page': pages.push(node); break
-    case 'folder':node.children.forEach(it => dfs(pages, it)); break
-    case 'separator': break
-  }
-}
+import {generateRssFeed, tree} from "../rss";
 
 export async function GET(request: Request, {
   params,
@@ -38,32 +7,15 @@ export async function GET(request: Request, {
 }) {
   const category = decodeURIComponent((await params).category);
   const feed = generateRssFeed(category);
-  const root = tree.children.find(it => it.type === 'folder' && it.name === category);
-  console.log("root", root)
-  if(!root || root.type !== 'folder') {
-    return new Response(feed.rss2(), {
-      headers: {
-        'content-type': 'application/xml',
-      },
+  const root = tree
+  if(!root) {
+    return new Response('404 not found', {
+      headers: { 'content-type': 'text/plain' },
+      status: 404
     })
   }
-  const pages = [] as Item[]
-  root.children.forEach(it => dfs(pages, it))
-  const ps = pages
-    .filter(it => it.type === 'page' && !it.external)
-    .map(it => source.getPage(it.url.replace('/', '').split('/')))
-    .toSorted((a,b) => (a?.data?.date ?? 0) - (b?.data?.date ?? 0))
-  ps.forEach(p => {
-    feed.addItem({
-      id: p!.data.bvid,
-      date: new Date(p!.data.date * 1000),
-      title: p!.data.title,
-      description: p!.data.description ?? "",
-      link: `${config.baseUrl}${p!.url}`,
-      content: p!.data.content,
-    })
-  })
-
+  // @ts-ignore
+  collectRSSPages(root, feed)
   return new Response(feed.rss2(), {
     headers: {
       'content-type': 'application/xml',
---
+++ fuma/src/app/rss/rss.ts
@@ -0,0 +1,50 @@
+import {config} from "../../../config";
+import {Feed} from "feed";
+import {source} from "@/lib/source";
+
+export function generateRssFeed(category: string) {
+  const site = config.baseUrl
+  const feedOptions = {
+    title: config.feed?.title ?? `${config.title} | RSS Feed`,
+    description: config.feed?.description ?? `${category} RSS feed powered by Auto2Doc`,
+    id: config.feed?.id ??  site,
+    link: config.feed?.link ?? site,
+    language: config.feed?.language ?? "zh-CN",
+    image: config.feed?.image ?? `${site}/logo.png`,
+    favicon: config.feed?.favicon ?? `${site}/favicon.ico`,
+    copyright: config.feed?.copyright ?? 'Auto2Doc',
+  };
+  const feed = new Feed(feedOptions);
+  return feed;
+}
+
+export const tree = source.getPageTree()
+
+type Node = (typeof tree.children)[number]
+export type Item = Node & { type: 'page' }
+export function dfs(pages: Item[], node: Node) {
+  switch (node.type) {
+    case 'page': pages.push(node); break
+    case 'folder':node.children.forEach(it => dfs(pages, it)); break
+    case 'separator': break
+  }
+}
+
+export function collectRSSPages(root: Node & {type: 'folder'}, feed: Feed) {
+  const pages = [] as Item[]
+  root.children.forEach(it => dfs(pages, it));
+  const ps = pages
+    .filter(it => it.type === 'page' && !it.external)
+    .map(it => source.getPage(it.url.replace('/', '').split('/')))
+    .toSorted((a,b) => (a?.data?.date ?? 0) - (b?.data?.date ?? 0))
+  ps.forEach(p => {
+    feed.addItem({
+      id: p!.data.bvid,
+      date: new Date(p!.data.date * 1000),
+      title: p!.data.title,
+      description: p!.data.description ?? "",
+      link: `${config.baseUrl}${p!.url}`,
+      content: p!.data.content,
+    })
+  })
+}
