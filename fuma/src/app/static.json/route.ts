37d279b6b0e1bdb7086101ec181266addabbc0be
ktKongTong <kt@ktlab.io>
2025-04-26 17:31:08 +0800

    feat: optimize content page retrieval and structure response for improved data handling

--- fuma/src/app/static.json/route.ts
+++ fuma/src/app/static.json/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server';
 import { source } from '@/lib/source';
-import type { OramaDocument } from 'fumadocs-core/search/orama-cloud';
 import { structure } from 'fumadocs-core/mdx-plugins';
 
 export const revalidate = false;
@@ -8,21 +7,22 @@ export const revalidate = false;
 export async function GET(): Promise<Response> {
   const pages = await source.getContentPages();
   const results = await Promise.all(
-    pages.map(async (page) => {
-
+    pages.flatMap(async (page) => {
       const structuredData = structure(page.data?.content!);
-      return {
-        id: page.url,
-        structured: structuredData,
+      const getHeading = (it?: string) => structuredData.headings.find(heading => heading.id === it)
+      return structuredData.contents.map((it, idx) => ({
+        id: `${page.url}-${idx + 1}`,
+        page_id: page.url,
+        section_id: it.heading,
+        section: getHeading(it.heading)?.content,
         tag: page.url.split('/')?.[1],
         url: page.url,
         title: page.data!.title,
-        // @ts-ignore
-        page_id: page.url,
+        content: it.content,
         description: page.data!.description,
-      } satisfies OramaDocument;
+      }))
     }),
   );
-
-  return NextResponse.json(results);
+  const res = results.flat()
+  return NextResponse.json(res);
 }
