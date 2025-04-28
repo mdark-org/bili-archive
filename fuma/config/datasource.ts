d763f17c0623fef5e8c2f4320e5b9ab0c3f0b3c6
ktKongTong <kt@ktlab.io>
2025-04-28 13:44:37 +0800

    feat: add new RSS feed categories and configurations for btnews

--- fuma/config/datasource.ts
+++ fuma/config/datasource.ts
@@ -5,6 +5,9 @@ const baseUrl = process.env.BASE_URL as string
 
 export const searchTags = [
   { name: '参考信息', value: 'refnews' },
+  { name: '睡前消息', value: 'btnews' },
+  { name: '高见', value: 'opinion' },
+  { name: '讲点黑话', value: 'slang' },
   { name: 'Koala聊开源', value: 'koala-oss' }
 ]
 
@@ -30,6 +33,78 @@ const datasources: Datasource[] = [
     transformers: {
       root: [
         addRSSPage(`${baseUrl}/rss/refnews`),
+        indexFolderTransformer
+      ],
+      folder:[ indexFolderTransformer ]
+    }
+  },
+  {
+    id: 'btnews.btnews',
+    name: '睡前消息',
+    description: "热河省蛮子，刘亦菲粉丝",
+    mountedPath: '/btnews',
+    category: [],
+    icon: "/image/btnews.png",
+    provider: 'unstorage',
+    config: {
+      includes: ['*.md', '*.mdx'],
+      driver: 'github',
+      repo: "mdark-org/btnews",
+      branch: "master",
+      dir: "/docs/btnews/btnews",
+      token: process.env.GITHUB_TOKEN,
+    },
+    transformers: {
+      root: [
+        addRSSPage(`${baseUrl}/rss/btnews`),
+        indexFolderTransformer
+      ],
+      folder:[ indexFolderTransformer ]
+    }
+  },
+  {
+    id: 'btnews.opinion',
+    name: '高见',
+    description: "热河省蛮子，刘亦菲粉丝",
+    mountedPath: '/opinion',
+    category: [],
+    icon: "/image/opinion.png",
+    provider: 'unstorage',
+    config: {
+      includes: ['*.md', '*.mdx'],
+      driver: 'github',
+      repo: "mdark-org/btnews",
+      branch: "master",
+      dir: "/docs/btnews/opinion",
+      token: process.env.GITHUB_TOKEN,
+    },
+    transformers: {
+      root: [
+        addRSSPage(`${baseUrl}/rss/opinion`),
+        indexFolderTransformer
+      ],
+      folder:[ indexFolderTransformer ]
+    }
+  },
+  {
+    id: 'btnews.slang',
+    name: '讲点黑话',
+    description: "讲点黑话",
+    mountedPath: '/slang',
+    category: [],
+    icon: "/image/slang.png",
+    provider: 'unstorage',
+    config: {
+      includes: ['*.md', '*.mdx'],
+      driver: 'github',
+      repo: "mdark-org/btnews",
+      branch: "master",
+      dir: "/docs/btnews/slang",
+      token: process.env.GITHUB_TOKEN,
+    },
+    transformers: {
+      root: [
+        addRSSPage(`${baseUrl}/rss/slang`),
         indexFolderTransformer
       ],
       folder:[ indexFolderTransformer ]
