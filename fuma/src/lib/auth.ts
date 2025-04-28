43f84a0ace84ac0edd3f5f46135318c83b18d72b
ktKongTong <kt@ktlab.io>
2025-04-29 06:42:51 +0800

    refactor: update output directory structure for builder types and modules

--- fuma/src/lib/auth.ts
+++ fuma/src/lib/auth.ts
@@ -14,10 +14,10 @@ export const auth = betterAuth({
     },
     usePlural: true,
   }),
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
-  },
+  // socialProviders: {
+  //   google: {
+  //     clientId: process.env.GOOGLE_CLIENT_ID as string,
+  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  //   },
+  // },
 })
