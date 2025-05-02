88250e4165d9030a58956b46852024ad71b5369f
ktKongTong <kt@ktlab.io>
2025-05-02 21:40:29 +0800

    feat: add new data source configuration for John Khan archive https://github.com/mdark-org/bili-john-khan

---
+++ fuma/datasource/sources/jhon-khan.ds.ts
@@ -0,0 +1,22 @@
+import {Context} from "../shared";
+import {addRSSPage, dateFolderTransformer} from "../transformer";
+
+export default (ctx: Context) => ({
+  id: 'archive.john-khan',
+  mountedPath: '/docs/john-khan',
+  category: [],
+  name: '小约翰可汗',
+  icon: "/image/john-khan.png",
+  description: "谁是周更up？！说话！look at me！",
+  provider: {
+    type: 'unstorage',
+    driver: 'github' as const,
+    options: { repo: "mdark-org/bili-john-khan", branch: "main", dir: "/docs/john-khan", token: ctx.env.GITHUB_TOKEN },
+    includes: ['*.md', '*.mdx'],
+  },
+  github: { repo: "mdark-org/bili-john-khan", branch: "main", dir: "/docs/john-khan" },
+  transformers: {
+    root: [ addRSSPage(`${ctx.env.BASE_URL}/rss/john-khan`) ],
+
+  }
+})
