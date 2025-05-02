513921c583dfbba2b5f431c16a1f3e8cf28c522d
ktKongTong <kt@ktlab.io>
2025-05-02 22:37:57 +0800

    feat: add new data source configuration for John Khan archive https://github.com/mdark-org/bili-john-khan

--- fuma/datasource/index.ts
+++ fuma/datasource/index.ts
@@ -4,6 +4,7 @@ import refnewsDs from "./sources/refnews.ds";
 import opinionDs from "./sources/opinion.ds";
 import slangDs from "./sources/slang.ds";
 import {Datasource} from "@repo/datasource/shared";
+import jhonKhanDs from "./sources/jhon-khan.ds";
 
 const ctx = {
   env: {
@@ -17,7 +18,8 @@ const datasources = [
   btnewsDs(ctx),
   opinionDs(ctx),
   slangDs(ctx),
-  koalaOssDs(ctx)
+  koalaOssDs(ctx),
+  jhonKhanDs(ctx)
 ] as Datasource[]
 
 
