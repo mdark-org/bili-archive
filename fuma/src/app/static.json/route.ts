887b2b590d91e29ee64d975eebcd7cb6bed80164
ktKongTong <kt@ktlab.io>
2025-06-02 02:29:31 +0800

    chore: diable static.json

--- fuma/src/app/static.json/route.ts
+++ fuma/src/app/static.json/route.ts
@@ -5,24 +5,24 @@ import { structure } from 'fumadocs-core/mdx-plugins';
 export const revalidate = false;
 
 export async function GET(): Promise<Response> {
-  const pages = await source.getContentPages();
-  const results = await Promise.all(
-    pages.flatMap(async (page) => {
-      const structuredData = structure(page.data?.content!);
-      const getHeading = (it?: string) => structuredData.headings.find(heading => heading.id === it)
-      return structuredData.contents.map((it, idx) => ({
-        id: `${page.url}-${idx + 1}`,
-        page_id: page.url,
-        section_id: it.heading,
-        section: getHeading(it.heading)?.content,
-        tag: page.url.split('/')?.[1],
-        url: page.url,
-        title: page.data!.title,
-        content: it.content,
-        description: page.data!.description,
-      }))
-    }),
-  );
-  const res = results.flat()
-  return NextResponse.json(res);
+  // const pages = await source.getContentPages();
+  // const results = await Promise.all(
+  //   pages.flatMap(async (page) => {
+  //     const structuredData = structure(page.data?.content!);
+  //     const getHeading = (it?: string) => structuredData.headings.find(heading => heading.id === it)
+  //     return structuredData.contents.map((it, idx) => ({
+  //       id: `${page.url}-${idx + 1}`,
+  //       page_id: page.url,
+  //       section_id: it.heading,
+  //       section: getHeading(it.heading)?.content,
+  //       tag: page.url.split('/')?.[1],
+  //       url: page.url,
+  //       title: page.data!.title,
+  //       content: it.content,
+  //       description: page.data!.description,
+  //     }))
+  //   }),
+  // );
+  // const res = results.flat()
+  return NextResponse.json([]);
 }
