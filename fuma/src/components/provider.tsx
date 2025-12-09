1da19e80d4bdbe1b09ed89c35c55938c73cb7586
ktkongtong <44502608+ktKongTong@users.noreply.github.com>
2025-12-09 14:25:43 +0800

    chore: upgrade fumadocs to 16 & nextjs to 16

--- fuma/src/components/provider.tsx
+++ fuma/src/components/provider.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { RootProvider } from 'fumadocs-ui/provider';
+import { RootProvider } from 'fumadocs-ui/provider/next';
 import type { ReactNode } from 'react';
 import type {SharedProps} from "fumadocs-ui/components/dialog/search";
 import OramaSearchDialog from "fumadocs-ui/components/dialog/search-orama";
